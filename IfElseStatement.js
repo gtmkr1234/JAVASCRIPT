d5b52693233bddbee96f7b8baa4b7fdee26c9603
priyanshu <priyanshusengarps@gmail.com>
2024-07-14 11:49:17 +0530

    javascript

--- IfElseStatement.js
+++ IfElseStatement.js
@@ -1,7 +1,10 @@
-const age=1;
-if(age>=18){
-console.log("You are Eligible.")
-}
-else{
-    console.log(`You are not Eligible ,${18-age} years are left to be Eligible for Driving License.`)
+const age = 1;
+if (age >= 18) {
+  console.log("You are Eligible.");
+} else {
+  console.log(
+    `You are not Eligible ,${
+      18 - age
+    } years are left to be Eligible for Driving License.`
+  );
 }
